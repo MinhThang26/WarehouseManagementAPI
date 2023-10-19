a35fe22a113314b7cd18a47d12d9f51fabd92e0a
MinhThang26 <thanghm.77.student@fit.tdc.edu.vn>
2023-10-19 11:20:39 +0700

    set up

--- index.js
+++ index.js
@@ -6,13 +6,16 @@ const morgan = require("morgan");
 const dotenv = require("dotenv");
 const app = express();
 
+dotenv.config();
 app.use(bodyParser.json({ limit: "50mb" }));
 app.use(cors());
 app.use(morgan("common"));
 
-app.get("/api", (req, res) => {
-  res.status(200).json("hello");
+mongoose.set("strictQuery", false);
+mongoose.connect(process.env.MONGODB_URL, () => {
+  console.log("connect mongodb");
 });
+
 app.listen(8080, () => {
   console.log("sever is running!");
 });
