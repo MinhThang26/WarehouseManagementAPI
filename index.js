97edf0916606c90e23b7d2fd0e7a1484c5a4eb2a
MinhThang26 <thanghm.77.student@fit.tdc.edu.vn>
2023-10-19 11:53:15 +0700

    set up

--- index.js
+++ index.js
@@ -3,19 +3,21 @@ const cors = require("cors");
 const mongoose = require("mongoose");
 var bodyParser = require("body-parser");
 const morgan = require("morgan");
-const dotenv = require("dotenv");
 const app = express();
 
-dotenv.config();
 app.use(bodyParser.json({ limit: "50mb" }));
 app.use(cors());
 app.use(morgan("common"));
+require("dotenv").config({ path: __dirname + "/.env" });
+const port = process.env.PORT;
 
-mongoose.set("strictQuery", false);
-mongoose.connect(process.env.MONGODB_URL, () => {
-  console.log("connect mongodb");
-});
-
-app.listen(8080, () => {
-  console.log("sever is running!");
+var dbConn = require("./config/databaseConfig");
+mongoose.connection.once("open", () => {
+  console.log("Connect mongodb");
+  app.listen(port, () => {
+    console.log("server api port " + port);
+    setInterval(function () {
+      console.log("server api port " + port);
+    }, 300000);
+  });
 });
