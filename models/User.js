9ce8544142f443ed471a733a80fc106abe2c0375
MinhThang26 <thanghm.77.student@fit.tdc.edu.vn>
2023-10-19 19:52:35 +0700

    up api loginUser && resgiterUser

---
+++ models/User.js
@@ -0,0 +1,36 @@
+const mongoose = require("mongoose");
+
+const userSchema = mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      minlength: 6,
+      maxlength: 20,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      minlength: 10,
+      maxlength: 30,
+    },
+    password: {
+      type: String,
+      minlength: 6,
+      required: true,
+    },
+    address: {
+      type: String,
+      required: true,
+    },
+    phone: {
+      type: Number,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("User", userSchema);
