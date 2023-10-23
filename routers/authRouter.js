c2414e81720d523593147707c31d86ee827284ba
MinhThang26 <thanghm.77.student@fit.tdc.edu.vn>
2023-10-23 14:37:52 +0700

    update API Register user

--- routers/authRouter.js
+++ routers/authRouter.js
@@ -5,8 +5,5 @@ const middlewareController = require("../controllers/middlewareController");
 router.post("/register", authController.registerUser);
 router.post("/login", authController.loginUser);
 router.post("/logout", middlewareController.verifyToken, authController.logout);
-router.get("/", (req, res) => {
-  console.log("hello");
-  res.status(200).json("hello");
-});
+
 module.exports = router;
