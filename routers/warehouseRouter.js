e0fb08efb4393d83c63bedf5a09412ccdaea5c51
Shjnro <21211TT3483@mail.tdc.edu.vn>
2023-10-26 14:36:04 +0700

    router

--- routers/warehouseRouter.js
+++ routers/warehouseRouter.js
@@ -1,11 +1,12 @@
 const router = require("express").Router();
 const warehouseCategoryController = require("../controllers/warehouseCategoryController");
 const middlewareController = require("../controllers/middlewareController");
+const WarehouseController = require("../controllers/warehouseController");
 
 router.get(
   "/category/list",
   middlewareController.verifyToken,
   warehouseCategoryController.getAllCategory
 );
-
+router.post("/addWarehouse",WarehouseController.addWarehouse);
 module.exports = router;
