e2239957e4eca72507fe306486e85c98cade86ea
MinhThang26 <thanghm.77.student@fit.tdc.edu.vn>
2023-11-02 14:45:56 +0700

    up US 013 update account

--- routers/authRouter.js
+++ routers/authRouter.js
@@ -1,9 +1,15 @@
 const router = require("express").Router();
+const uploadCloud = require("../config/cloudinaryConfig");
 const authController = require("../controllers/authController");
 const middlewareController = require("../controllers/middlewareController");
 
 router.post("/register", authController.register);
 router.post("/login", authController.login);
 router.get("/logout", middlewareController.verifyToken, authController.logout);
+router.post(
+  "/update-account",
+  uploadCloud.single("image"),
+  authController.updateAccount
+);
 
 module.exports = router;
