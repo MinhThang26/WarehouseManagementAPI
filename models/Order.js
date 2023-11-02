1a6cb8bb98595338e4a22b60399085d9c35673e1
Shjnro <21211TT3483@mail.tdc.edu.vn>
2023-11-02 21:40:27 +0700

    create order model

---
+++ models/Order.js
@@ -0,0 +1,37 @@
+const mongoose = require("mongoose");
+
+const orderSchema = mongoose.Schema(
+    {
+        money: {
+            type: Number,
+            required: true,
+            
+        },
+          owner: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "Owner",
+            required: true,
+        },
+        user: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User",
+            required: true,
+        }, 
+        warehouses: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "Warehouse",
+        },
+        rentalTime:{
+            type: String,
+            require: true,
+        },
+
+        isActive: {
+            type: Boolean,
+            default: true,
+          },
+    },
+    { timestamps: true }
+);
+
+module.exports = mongoose.model("Order", orderSchema);
