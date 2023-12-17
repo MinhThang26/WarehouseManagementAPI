01ca8d9340ac427ffae753f5ca704d90ad14847d
KHAI-DO <dnmk26boxer@gmail.com>
2023-12-17 21:43:23 +0700

    abc

--- index.js
+++ index.js
@@ -31,10 +31,10 @@ mongoose.connection.once("open", () => {
 });
 
 io.on("connection", (socket) => {
-  console.log("A user connected");
-
+  console.log("A user connected " + socket.id);
+  
   socket.on("disconnect", () => {
-    console.log("A user disconnected");
+    console.log("A user disconnected " + socket.id);
   });
 });
 
